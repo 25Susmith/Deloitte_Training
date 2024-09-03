c6184131ced2f07f2740b34a1cf955c47477ed76
25Susmith <119886351+25Susmith@users.noreply.github.com>
2024-09-03 17:56:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int eid;
+            string ename;
+            string ejob;
+            int sal;
+
+            Console.WriteLine("Enter ID: ");
+            eid = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter name: ");
+            ename = Console.ReadLine();
+
+            Console.WriteLine("Enter job: ");
+            ejob = Console.ReadLine();
+
+            Console.WriteLine("Enter salary: ");
+            sal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-------Emp Details--------");
+            Console.WriteLine("Emp ID: " + eid);
+            Console.WriteLine("Emp Name: " + ename);
+            Console.WriteLine("Emp Job: " + ejob);
+            Console.WriteLine("Emp salary: " + sal);
+
+            Console.ReadLine();
+        }
+    }
+}
