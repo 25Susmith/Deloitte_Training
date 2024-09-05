30107c738ae6e8d3f99e88f6cd7b16f4f3bedf8f
25Susmith <119886351+25Susmith@users.noreply.github.com>
2024-09-05 14:51:05 +0530

    Add files via upload

---
+++ Day3_.NET/assign1_connected.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "Server=USHYDSSUSMITH6; Database=Sept2024Db; Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connStr);
+            // conn.ConnectionString = connStr;
+
+            string cmdText = "insert into pro VALUES(7651, 't shirt', 'clothes', 400, 5)";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+
+            conn.Open();
+            Console.WriteLine("Connected to Database successfully");
+
+            int n = cmd.ExecuteNonQuery();  // For DML:  Insert, update, delete
+
+            Console.WriteLine("No. of rows affected :" + n);
+            conn.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day3_.NET/assign1_disconnected.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = "Server=USHYDSSUSMITH6; Database=Sept2024Db; Integrated Security=true";
+            string cmdText = "SELECT * FROM pro";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmdText, connStr);
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds);
+
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                Console.WriteLine("{0},{1},{2},{3},{4}", row["pid"].ToString(), row["pname"].ToString(), row["ctg"].ToString(), row["price"].ToString(), row["qty"].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
